3ecc3ba976093274ad182eb10a64b0f3db79b401
shuyu <shuyu.han@cern.ch>
2025-12-30 18:33:45 +0800

    Test finished.

--- Dog.cpp
+++ Dog.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 /**
  * @brief 每一个对象都是一只可爱的小狗
@@ -8,6 +10,7 @@ class Dog
 public:
     // =======众狗之神有什么=======
     inline static int dog_number = 0;// 现在一共有几只小狗？小狗不该操心这个，这是众狗之神管的事情，所以不在小狗对象中，一开始有0只
+    inline static std::vector<Dog*> dog_list;
 
     // =======每一只小狗对象有些什么=======
     std::string name;// 小狗的名字
@@ -21,10 +24,18 @@ public:
         this->age = age;
         std::cout << "My name is " << name << ", I'm " << age << " years old." << std::endl;
         // @todo 还需要干什么吗？这只小狗好像还不在众狗之神的花名册里
+        dog_number += 1;
+        dog_list.push_back(this);
     }
     ~Dog() { // 消灭小狗时要做什么，如果小狗没了，需要开追悼会，好吧，有点地狱，好在只是赛博小狗
-        std::cout << "Goodbye " << name << "." << std::endl;
+        std::cout << "Goodbye, " << name << "." << std::endl;
         // @todo 少了点什么
+        // 从列表中移除这只小狗
+        auto it = std::find(dog_list.begin(), dog_list.end(), this);
+        if (it != dog_list.end()) {
+            dog_list.erase(it);
+        }
+        dog_number -= 1;
     }
 
     /**
@@ -32,36 +43,60 @@ public:
      */
     void have_birthday() {
         std::cout << name << " is eating birthday cake ..." << std::endl;
-        std::cout << "I'm " << age << "years old now." << std::endl;
         // @todo
+        age +=1;
+        std::cout << name << " is " << age << " years old now." << std::endl;
+        
     }
 
     /**
      * @brief 同样是过生日，但这次是给其他小狗过
      */
-    void have_birthday(Dog other) { // 这里对吗？哎呀，我好像不小心把该过生日的小狗复制了一份，它现在变成克隆狗了，这可不好
+    void have_birthday(Dog& other) { // 这里对吗？哎呀，我好像不小心把该过生日的小狗复制了一份，它现在变成克隆狗了，这可不好
         std::cout << "Happy Birthday! " << other.name << "." << std::endl;
         // @todo 这里好像少了点什么？
+        other.have_birthday();
     }
 
     /**
      * @brief 比年龄大小，这只小狗想要比比它和同伴谁更大一些？
      * @return 如果这只狗更大，返回true，反之，返回false
      */
-    bool older_than(Dog& other) {}
+    bool older_than(Dog& other) {
+        return ( (this->age) > other.age );
+    }
 
     /**
      * @brief 众狗之神啊！你来裁决一下我面前的这两只狗谁大谁小吧！
      */
-    static Dog older_dog(Dog& dog1, Dog& dog2) { // 哎呀，我好像又克隆了一只小狗，年龄大的小狗会被克隆一份，怎么回事呢？
+    static Dog& older_dog(Dog& dog1, Dog& dog2) { // 哎呀，我好像又克隆了一只小狗，年龄大的小狗会被克隆一份，怎么回事呢？
         // @todo
+        //std::cout << dog1.name << " is " << dog1.age << " years old now." << std::endl;
+        //std::cout << dog2.name << " is " << dog2.age << " years old now." << std::endl;
+        if (dog1.age > dog2.age)
+        {
+            return dog1;
+        }else if (dog2.age > dog1.age)
+        {
+            return dog2;
+        }else
+        {
+            std::cout << "Gougou is a bad dog." << std::endl;
+            return dog1;
+        }
     }
 
     /**
      * @brief 挑战：众狗之神想要给所有小狗过生日，你能帮帮它吗？
      * 提示：可能需要给众狗之神一个花名册，让它可以随时找到所有小狗，
-     * <array>好像挺合适的，std::array::push_back()方法可以在数组后面添加元素哦
+     * <vector>好像挺合适的，std::vector::push_back()方法可以在数组后面添加元素哦
+     * 注意：std::vector<你要在数组里放什么东西> 这么去用哦
      */
     static void have_birthday_to_all() {
+        for (int i = 0; i < dog_number-1; i++)
+        {
+            dog_list[i] -> have_birthday(*dog_list[i+1]);
+        }
+        dog_list[dog_number-1] -> have_birthday(*dog_list[0]);
     }
 };
--- main.cpp
+++ main.cpp
@@ -8,20 +8,28 @@ int main() {
     Dog dog2("西米露", 1);
 
     // @todo 众狗之神需要统计现在有几只小狗
-    std::cout << "There're " << "dogs here." << std::endl;
+    std::cout << "There're " << Dog::dog_number << " dogs here." << std::endl;
 
-    // @todo 西卡给西米露过生日
-    dog1.age += 1; // 对，对吗？好粗暴，西卡生日蛋糕都没吃，生日就过去了
+    // @todo 西米露给西卡过生日
+    dog2.have_birthday(dog1);
+    //dog1.age += 1; // 对，对吗？好粗暴，西卡生日蛋糕都没吃，生日就过去了
 
     // @todo 西米露要和西卡比大小
     std::cout << "Am I oldder than you? True or false?" << std::endl;
+    std::cout << dog2.older_than(dog1) << std::endl;
 
     // @todo 为了报复西米露不给自己过生日，西卡决定给西米露过一百个生日
+    for (int i = 0; i < 100; i++)
+    {
+        dog1.have_birthday(dog2);
+    }
 
     // @todo 现在西卡和西米露谁大？
-    std::cout << "The oldest dog is " << " now." << std::endl;
+    std::cout << "The oldest dog is " << Dog::older_dog(dog1, dog2).name << " now." << std::endl;
 
     // @todo 众狗之神决定给所有小狗过生日
+    //dog1.have_birthday(dog2);
+    //dog2.have_birthday(dog1);
     Dog::have_birthday_to_all();
 
     return 0;
